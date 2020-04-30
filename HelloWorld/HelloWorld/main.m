d1a237032549baa8ba44467eae135e2a5c44b77d
Tejas Mehta <tj@tejas-mehta.com>
2020-04-29 21:08:55 -0700

    A MAJOR update

--- HelloWorld/HelloWorld/main.m
+++ HelloWorld/HelloWorld/main.m
@@ -12,6 +12,7 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        NSLog(@"One small leap for a TOG");
     }
     return 0;
 }
