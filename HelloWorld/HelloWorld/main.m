397b307890b7b14cf69c69c83701af07a95ce4fb
Tejas Mehta <tj@tejas-mehta.com>
2020-04-29 20:46:41 -0700

    init commit

---
+++ HelloWorld/HelloWorld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Tejas Mehta on 4/29/20.
+//  Copyright © 2020 Tejas Mehta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
